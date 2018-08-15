07a20a991bce40975b8312255a6f8a73194ae55b
rogerwschmidt <rogerschmidt@gmail.com>
2018-08-15 07:24:14 -0400

    clarification

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      // array of states goes here
+    }
+  }
   render() {
     return (
       <div>
